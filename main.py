93c0ca14a82b39699810ed3f06ad1d76f93275bc
yohanfaj <yohan.fajerman@efrei.net>
2021-05-21 10:20:05 +0200

    removed constants.py (compatibility issues)

--- main.py
+++ main.py
@@ -2,15 +2,60 @@ import pygame, sys, time, random
 from pygame.locals import *
 pygame.mixer.pre_init(44100, -16, 2, 512)
 import assets as ast
-import constants as cst
 import functions as fc
 
 # CONSTANTS
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+TEXTCOLOR = (0, 0, 0)
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+FPS = 60
+PLAYERMOVERATE_X = 4
+PLAYERMOVERATE_Y = 10
+tomato1moverate = tomato2moverate = tomato3moverate = 0
+tomato_timer = 0
+
+HEALTH = 3
+BONUS = 4000
+bonus_cpt = 0
+isJump = False
+tomato1jump = tomato2jump = tomato3jump = False
+finalscore = 0
+
+# Plateform
+P1_X = 0
+P1_Y = 590
+P1_LX = 600
+P_Y = 10
+P1 = pygame.Rect(P1_X, P1_Y, P1_LX, P_Y)
+isOnP1 = False
+P2_X = 0
+P2_Y = 470
+P2_LX = 450
+P2 = pygame.Rect(P2_X, P2_Y, P2_LX, P_Y)
+isOnP2 = False
+P3_X = 150
+P3_Y = 350
+P3_LX = 450
+P3 = pygame.Rect(P3_X, P3_Y, P3_LX, P_Y)
+isOnP3 = False
+P4_X = 0
+P4_Y = 230
+P4_LX = 450
+P4 = pygame.Rect(P4_X, P4_Y, P4_LX, P_Y)
+isOnP4 = False
+
 
 # Set up pygame, the window, and the mouse cursor.
 pygame.init()
 mainClock = pygame.time.Clock()
-windowSurface = pygame.display.set_mode((cst.WINDOWWIDTH, cst.WINDOWHEIGHT))
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pygame.display.set_caption("Revaniv's Tower")
 pygame.mouse.set_visible(True)
 
@@ -50,15 +95,15 @@ while running:
                     moveLeft = False
                     moveRight = True
                     player = ast.run_right_list[0]
-                if event.key == K_SPACE and cst.isJump == False:
+                if event.key == K_SPACE and isJump == False:
                     if event.key == K_LEFT or event.key == K_a:
                         player = pygame.image.load("Assets/core_character_jump_left.png")
                     elif event.key == K_RIGHT or event.key == K_d:
                         player = pygame.image.load("Assets/core_character_jump_right.png")
-                    cst.isJump = True
+                    isJump = True
                     t = 0
                     y_basis = ast.playerRect.y
-                    y_speed = cst.PLAYERMOVERATE_Y
+                    y_speed = PLAYERMOVERATE_Y
 
                     # OLD VERSION :
                     # y_basis = playerRect.y
@@ -94,9 +139,9 @@ while running:
 
         # Move the player around.
         if moveLeft and ast.playerRect.left > 0:
-            ast.playerRect.left -= cst.PLAYERMOVERATE_X
-        if moveRight and ast.playerRect.right < cst.WINDOWWIDTH:
-            ast.playerRect.right += cst.PLAYERMOVERATE_X
+            ast.playerRect.left -= PLAYERMOVERATE_X
+        if moveRight and ast.playerRect.right < WINDOWWIDTH:
+            ast.playerRect.right += PLAYERMOVERATE_X
 
         # if isJump is True:
         #    playerRect.y -= PLAYERMOVERATE_Y * 2
@@ -106,82 +151,82 @@ while running:
         #        PLAYERMOVERATE_Y = 10
 
         # Jump mechanics
-        if cst.isJump:
+        if isJump:
             ast.playerRect.y = fc.y_trajectory(y_basis, y_speed, t)
             t += 1
 
-            if ast.playerRect.colliderect(cst.P1):
-                if ast.playerRect.y > cst.P1_Y:
+            if ast.playerRect.colliderect(P1):
+                if ast.playerRect.y > P1_Y:
                     y_speed = 0
                     t = 0
                     y_basis = ast.playerRect.y + 10
-                elif ast.playerRect.y <= cst.P1_Y:
-                    cst.isJump = False
-                    ast.playerRect.y = cst.P1_Y - 50
-                    cst.isOnP1 = True
-                if cst.P1_Y + cst.P_Y > ast.playerRect.y > cst.P1_Y - 49:
-                    cst.isOnP1 = False
+                elif ast.playerRect.y <= P1_Y:
+                    isJump = False
+                    ast.playerRect.y = P1_Y - 50
+                    isOnP1 = True
+                if P1_Y + P_Y > ast.playerRect.y > P1_Y - 49:
+                    isOnP1 = False
 
-            if ast.playerRect.colliderect(cst.P2):
-                if ast.playerRect.y > cst.P2_Y:
+            if ast.playerRect.colliderect(P2):
+                if ast.playerRect.y > P2_Y:
                     y_speed = 0
                     t = 0
                     y_basis = ast.playerRect.y + 10
-                elif ast.playerRect.y <= cst.P2_Y:
-                    cst.isJump = False
-                    ast.playerRect.y = cst.P2_Y - 50
-                    cst.isOnP2 = True
-                if cst.P2_Y + cst.P_Y > ast.playerRect.y > cst.P2_Y - 49:
-                    cst.isOnP2 = False
+                elif ast.playerRect.y <= P2_Y:
+                    isJump = False
+                    ast.playerRect.y = P2_Y - 50
+                    isOnP2 = True
+                if P2_Y + P_Y > ast.playerRect.y > P2_Y - 49:
+                    isOnP2 = False
 
-            if ast.playerRect.colliderect(cst.P3):
-                if ast.playerRect.y > cst.P3_Y:
+            if ast.playerRect.colliderect(P3):
+                if ast.playerRect.y > P3_Y:
                     y_speed = 0
                     t = 0
                     y_basis = ast.playerRect.y + 10
-                elif ast.playerRect.y <= cst.P3_Y:
-                    cst.isJump = False
-                    ast.playerRect.y = cst.P3_Y - 50
-                    cst.isOnP3 = True
-                if cst.P3_Y + cst.P_Y > ast.playerRect.y > cst.P3_Y - 49:
-                    cst.isOnP3 = False
+                elif ast.playerRect.y <= P3_Y:
+                    isJump = False
+                    ast.playerRect.y = P3_Y - 50
+                    isOnP3 = True
+                if P3_Y + P_Y > ast.playerRect.y > P3_Y - 49:
+                    isOnP3 = False
 
-            if ast.playerRect.colliderect(cst.P4):
-                if ast.playerRect.y > cst.P4_Y:
+            if ast.playerRect.colliderect(P4):
+                if ast.playerRect.y > P4_Y:
                     y_speed = 0
                     t = 0
                     y_basis = ast.playerRect.y + 10
-                elif ast.playerRect.y <= cst.P4_Y:
-                    cst.isJump = False
-                    ast.playerRect.y = cst.P4_Y - 50
-                    cst.isOnP4 = True
-                if cst.P4_Y + cst.P_Y > ast.playerRect.y > cst.P4_Y - 49:
-                    cst.isOnP4 = False
+                elif ast.playerRect.y <= P4_Y:
+                    isJump = False
+                    ast.playerRect.y = P4_Y - 50
+                    isOnP4 = True
+                if P4_Y + P_Y > ast.playerRect.y > P4_Y - 49:
+                    isOnP4 = False
 
-        if cst.isOnP1 and not (cst.P1_X - 25 < ast.playerRect.x < cst.P1_X + cst.P1_LX):
-            cst.isOnP1 = False
-            cst.isJump = True
+        if isOnP1 and not (P1_X - 25 < ast.playerRect.x < P1_X + P1_LX):
+            isOnP1 = False
+            isJump = True
             y_speed = 0
             y_basis = ast.playerRect.y
             t = 0
 
-        if cst.isOnP2 and not (cst.P2_X - 25 < ast.playerRect.x < cst.P2_X + cst.P2_LX):
-            cst.isOnP2 = False
-            cst.isJump = True
+        if isOnP2 and not (P2_X - 25 < ast.playerRect.x < P2_X + P2_LX):
+            isOnP2 = False
+            isJump = True
             y_speed = 0
             y_basis = ast.playerRect.y
             t = 0
 
-        if cst.isOnP3 and not (cst.P3_X - 25 < ast.playerRect.x < cst.P3_X + cst.P3_LX):
-            cst.isOnP3 = False
-            cst.isJump = True
+        if isOnP3 and not (P3_X - 25 < ast.playerRect.x < P3_X + P3_LX):
+            isOnP3 = False
+            isJump = True
             y_speed = 0
             y_basis = ast.playerRect.y
             t = 0
 
-        if cst.isOnP4 and not (cst.P4_X - 25 < ast.playerRect.x < cst.P4_X + cst.P4_LX):
-            cst.isOnP4 = False
-            cst.isJump = True
+        if isOnP4 and not (P4_X - 25 < ast.playerRect.x < P4_X + P4_LX):
+            isOnP4 = False
+            isJump = True
             y_speed = 0
             y_basis = ast.playerRect.y
             t = 0
@@ -193,135 +238,135 @@ while running:
         # badLeft = False
         # badRect.right += BADDIEMOVERATE
 
-        ast.tomato1rect.x += cst.tomato1moverate
+        ast.tomato1rect.x += tomato1moverate
         if (not (585 >= ast.tomato1rect.x >= 0) and ast.tomato1rect.y < 550) or (ast.tomato1rect.y > 550 and ast.tomato1rect.x >= 585):
-            ast.tomato1rect.x -= cst.tomato1moverate
-            cst.tomato1moverate = -cst.tomato1moverate
-        if not (ast.tomato1rect.colliderect(cst.P1) or ast.tomato1rect.colliderect(cst.P2) or ast.tomato1rect.colliderect(
-                cst.P3) or ast.tomato1rect.colliderect(cst.P4)) and not cst.tomato1jump:
-            cst.tomato1jump = True
+            ast.tomato1rect.x -= tomato1moverate
+            tomato1moverate = -tomato1moverate
+        if not (ast.tomato1rect.colliderect(P1) or ast.tomato1rect.colliderect(P2) or ast.tomato1rect.colliderect(
+                P3) or ast.tomato1rect.colliderect(P4)) and not tomato1jump:
+            tomato1jump = True
             tomato1_y_basis = ast.tomato1rect.y
             t_tomato1 = 0
-        if (ast.tomato1rect.colliderect(cst.P1) or ast.tomato1rect.colliderect(cst.P2) or ast.tomato1rect.colliderect(
-                cst.P3) or ast.tomato1rect.colliderect(cst.P4)) and cst.tomato1jump:
+        if (ast.tomato1rect.colliderect(P1) or ast.tomato1rect.colliderect(P2) or ast.tomato1rect.colliderect(
+                P3) or ast.tomato1rect.colliderect(P4)) and tomato1jump:
             tomato1jump = False
             ast.tomato1rect.y -= 10
 
-        if cst.tomato1jump:
+        if tomato1jump:
             ast.tomato1rect.y = fc.y_trajectory(tomato1_y_basis, 0, t_tomato1)
             t_tomato1 += 1
 
-        ast.tomato2rect.x += cst.tomato2moverate
+        ast.tomato2rect.x += tomato2moverate
         if (not (585 >= ast.tomato2rect.x >= 0) and ast.tomato2rect.y < 550) or (ast.tomato2rect.y > 550 and ast.tomato2rect.x >= 585):
-            ast.tomato2rect.x -= cst.tomato2moverate
-            cst.tomato2moverate = -cst.tomato2moverate
-        if not (ast.tomato2rect.colliderect(cst.P1) or ast.tomato2rect.colliderect(cst.P2) or ast.tomato2rect.colliderect(
-                cst.P3) or ast.tomato2rect.colliderect(cst.P4)) and not cst.tomato2jump:
-            cst.tomato2jump = True
+            ast.tomato2rect.x -= tomato2moverate
+            tomato2moverate = -tomato2moverate
+        if not (ast.tomato2rect.colliderect(P1) or ast.tomato2rect.colliderect(P2) or ast.tomato2rect.colliderect(
+                P3) or ast.tomato2rect.colliderect(P4)) and not tomato2jump:
+            tomato2jump = True
             tomato2_y_basis = ast.tomato2rect.y
             t_tomato2 = 0
-        if (ast.tomato2rect.colliderect(cst.P1) or ast.tomato2rect.colliderect(cst.P2) or ast.tomato2rect.colliderect(
-                cst.P3) or ast.tomato2rect.colliderect(cst.P4)) and cst.tomato2jump:
-            cst.tomato2jump = False
+        if (ast.tomato2rect.colliderect(P1) or ast.tomato2rect.colliderect(P2) or ast.tomato2rect.colliderect(
+                P3) or ast.tomato2rect.colliderect(P4)) and tomato2jump:
+            tomato2jump = False
             ast.tomato2rect.y -= 10
 
-        if cst.tomato2jump:
+        if tomato2jump:
             ast.tomato2rect.y = fc.y_trajectory(tomato2_y_basis, 0, t_tomato2)
             t_tomato2 += 1
 
-        ast.tomato3rect.x += cst.tomato3moverate
+        ast.tomato3rect.x += tomato3moverate
         if (not (585 >= ast.tomato3rect.x >= 0) and ast.tomato3rect.y < 550) or (ast.tomato3rect.y > 550 and ast.tomato3rect.x >= 585):
-            ast.tomato3rect.x -= cst.tomato3moverate
-            cst.tomato3moverate = -cst.tomato3moverate
-        if not (ast.tomato3rect.colliderect(cst.P1) or ast.tomato3rect.colliderect(cst.P2) or ast.tomato3rect.colliderect(
-                cst.P3) or ast.tomato3rect.colliderect(cst.P4)) and not cst.tomato3jump:
-            cst.tomato3jump = True
+            ast.tomato3rect.x -= tomato3moverate
+            tomato3moverate = -tomato3moverate
+        if not (ast.tomato3rect.colliderect(P1) or ast.tomato3rect.colliderect(P2) or ast.tomato3rect.colliderect(
+                P3) or ast.tomato3rect.colliderect(P4)) and not tomato3jump:
+            tomato3jump = True
             tomato3_y_basis = ast.tomato3rect.y
             t_tomato3 = 0
-        if (ast.tomato3rect.colliderect(cst.P1) or ast.tomato3rect.colliderect(cst.P2) or ast.tomato3rect.colliderect(
-                cst.P3) or ast.tomato3rect.colliderect(cst.P4)) and cst.tomato3jump:
+        if (ast.tomato3rect.colliderect(P1) or ast.tomato3rect.colliderect(P2) or ast.tomato3rect.colliderect(
+                P3) or ast.tomato3rect.colliderect(P4)) and tomato3jump:
             tomato3jump = False
             ast.tomato3rect.y -= 10
 
-        if cst.tomato3jump:
+        if tomato3jump:
             ast.tomato3rect.y = fc.y_trajectory(tomato3_y_basis, 0, t_tomato3)
             t_tomato3 += 1
 
-        cst.tomato_timer += 1
+        tomato_timer += 1
 
-        if cst.tomato_timer == 60:
+        if tomato_timer == 60:
             tomato1moverate = 4
-        if cst.tomato_timer == 260:
+        if tomato_timer == 260:
             tomato2moverate = 4
-        if cst.tomato_timer == 460:
+        if tomato_timer == 460:
             tomato3moverate = 4
 
         if (ast.tomato1rect.y > 500 and ast.tomato1rect.x < -15):
             ast.tomato1rect.x = 50
             ast.tomato1rect.y = 190
             tomato1jump = False
-            tomato1moverate = -cst.tomato1moverate
+            tomato1moverate = -tomato1moverate
         if (ast.tomato2rect.y > 500 and ast.tomato2rect.x < -15):
             ast.tomato2rect.x = 50
             ast.tomato2rect.y = 190
             tomato2jump = False
-            tomato2moverate = -cst.tomato2moverate
+            tomato2moverate = -tomato2moverate
         if (ast.tomato3rect.y > 500 and ast.tomato3rect.x < -15):
             ast.tomato3rect.x = 50
             ast.tomato3rect.y = 190
             tomato3jump = False
-            tomato3moverate = -cst.tomato3moverate
+            tomato3moverate = -tomato3moverate
 
-        if cst.HEALTH == 0:
+        if HEALTH == 0:
             pygame.mixer.music.stop()
             ast.GameOverSound.play()
             fc.displayScreen(ast.GameOverScreen)
             fc.waitForPlayerToPressKey()
             fc.terminate()
 
-        cst.bonus_cpt += 1
-        if cst.bonus_cpt > 150:
-            cst.BONUS -= 100
-            cst.bonus_cpt = 0
-        if cst.BONUS == 1000:
+        bonus_cpt += 1
+        if bonus_cpt > 150:
+            BONUS -= 100
+            bonus_cpt = 0
+        if BONUS == 1000:
             pygame.mixer.music.stop()
             ast.TimeRun.set_volume(0.5)
             ast.TimeRun.play()
             pygame.mixer.music.play(-1, 0.0, 3000)
-        if cst.BONUS < 0:
-            cst.BONUS = 0
+        if BONUS < 0:
+            BONUS = 0
 
-        if cst.BONUS <= 3700:
+        if BONUS <= 3700:
             windowSurface.blit(ast.potion, ast.potionRect)
             if ast.playerRect.colliderect(ast.potionRect):
-                cst.HEALTH += 1
+                HEALTH += 1
               
 
 
-        if ast.playerRect.colliderect(ast.tomato1rect) or ast.playerRect.colliderect(ast.tomato2rect) or ast.playerRect.colliderect(ast.tomato3rect) or cst.BONUS == 0:
-            cst.tomato1jump = cst.tomato2jump = cst.tomato3jump = False
+        if ast.playerRect.colliderect(ast.tomato1rect) or ast.playerRect.colliderect(ast.tomato2rect) or ast.playerRect.colliderect(ast.tomato3rect) or BONUS == 0:
+            tomato1jump = tomato2jump = tomato3jump = False
             pygame.mixer.music.stop()
             ast.HitSound.play()
             fc.waitForPlayerToPressKey()
             ast.playerRect.update(0, 541, 25, 49)
-            cst.isJump = False
+            isJump = False
             ast.badRect.update(50, 180, 50, 50)
             ast.tomato1rect.update(50, 190, 15, 15)
             ast.tomato2rect.update(50, 190, 15, 15)
             ast.tomato3rect.update(50, 190, 15, 15)
-            cst.tomato1moverate = cst.tomato2moverate = cst.tomato3moverate = 0
-            cst.tomato_timer = 0
+            tomato1moverate = tomato2moverate = tomato3moverate = 0
+            tomato_timer = 0
             moveLeft = moveRight = False
             ast.HitSound.stop()
             pygame.mixer.music.play(-1, 0.0, 3000)
-            cst.HEALTH -= 1
+            HEALTH -= 1
             BONUS = 5000
 
         if ast.playerRect.colliderect(ast.badRect):
             moveLeft = moveRight = False
-            cst.tomato1moverate = cst.tomato2moverate = cst.tomato3moverate = 0
-            cst.tomato1jump = cst.tomato2jump = cst.tomato3jump = False
-            cst.finalscore = cst.BONUS + 1000*cst.HEALTH
+            tomato1moverate = tomato2moverate = tomato3moverate = 0
+            tomato1jump = tomato2jump = tomato3jump = False
+            finalscore = BONUS + 1000*HEALTH
             pygame.mixer.music.stop()
             ast.WinSound.play()
             fc.displayWinScreen(ast.Win_screen)
@@ -329,4 +374,4 @@ while running:
             fc.terminate()
 
         fc.redrawScreen()
-        mainClock.tick(cst.FPS)
+        mainClock.tick(FPS)
