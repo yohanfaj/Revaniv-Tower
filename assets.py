aecaeff28ccd4943dc750a5051aa37ce29fee592
yohanfaj <yohan.fajerman@efrei.net>
2021-05-21 11:58:20 +0200

    removed constants attributes in assets & functions files

--- assets.py
+++ assets.py
@@ -1,9 +1,54 @@
 import pygame, sys, time, random
 from pygame.locals import *
 pygame.mixer.pre_init(44100, -16, 2, 512)
-import constants as cst
-
 pygame.init()
+
+
+# CONSTANTS & VARIABLES
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+FPS = 60
+PLAYERMOVERATE_X = 4
+PLAYERMOVERATE_Y = 10
+tomato1moverate = tomato2moverate = tomato3moverate = 0
+tomato_timer = 0
+
+HEALTH = 3
+BONUS = 4000
+bonus_cpt = 0
+isJump = False
+tomato1jump = tomato2jump = tomato3jump = False
+finalscore = 0
+
+# Plateform
+P1_X = 0
+P1_Y = 590
+P1_LX = 600
+P_Y = 10
+P1 = pygame.Rect(P1_X, P1_Y, P1_LX, P_Y)
+isOnP1 = False
+P2_X = 0
+P2_Y = 470
+P2_LX = 450
+P2 = pygame.Rect(P2_X, P2_Y, P2_LX, P_Y)
+isOnP2 = False
+P3_X = 150
+P3_Y = 350
+P3_LX = 450
+P3 = pygame.Rect(P3_X, P3_Y, P3_LX, P_Y)
+isOnP3 = False
+P4_X = 0
+P4_Y = 230
+P4_LX = 450
+P4 = pygame.Rect(P4_X, P4_Y, P4_LX, P_Y)
+isOnP4 = False
+
+
+# ASSETS
 
 # Set up the fonts.
 font = pygame.font.Font('Assets/fette-unz-fraktur.ttf', 30)
@@ -52,7 +97,7 @@ tomato3rect = pygame.Rect(50, 190, 15, 15)
 bg = pygame.image.load("Assets/map_cantine.jpeg")
 potion = pygame.image.load("Assets/potion.png")
 potionRect = potion.get_rect()
-potion_y = {0: cst.P_Y, 1: cst.P1_Y, 2: cst.P2_Y, 3: cst.P3_Y}
+potion_y = {0: P_Y, 1: P1_Y, 2: P2_Y, 3: P3_Y}
 potionRect.x = random.randint(0, 600)
 potionRect.y = potion_y[random.randint(0, 3)]+30
 
--- functions.py
+++ functions.py
@@ -1,13 +1,57 @@
 import pygame, sys, random
 from pygame.locals import *
 pygame.mixer.pre_init(44100, -16, 2, 512)
-import constants as cst
 import assets as ast
 
 pygame.init()
+
+# CONSTANTS & VARIABLES:
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+FPS = 60
+PLAYERMOVERATE_X = 4
+PLAYERMOVERATE_Y = 10
+tomato1moverate = tomato2moverate = tomato3moverate = 0
+tomato_timer = 0
+
+HEALTH = 3
+BONUS = 4000
+bonus_cpt = 0
+isJump = False
+tomato1jump = tomato2jump = tomato3jump = False
+finalscore = 0
+
+# Plateform
+P1_X = 0
+P1_Y = 590
+P1_LX = 600
+P_Y = 10
+P1 = pygame.Rect(P1_X, P1_Y, P1_LX, P_Y)
+isOnP1 = False
+P2_X = 0
+P2_Y = 470
+P2_LX = 450
+P2 = pygame.Rect(P2_X, P2_Y, P2_LX, P_Y)
+isOnP2 = False
+P3_X = 150
+P3_Y = 350
+P3_LX = 450
+P3 = pygame.Rect(P3_X, P3_Y, P3_LX, P_Y)
+isOnP3 = False
+P4_X = 0
+P4_Y = 230
+P4_LX = 450
+P4 = pygame.Rect(P4_X, P4_Y, P4_LX, P_Y)
+isOnP4 = False
+
 
 
 # Functions:
@@ -35,14 +79,14 @@ def redrawScreen():
     windowSurface.blit(ast.tomato1, ast.tomato1rect)
     windowSurface.blit(ast.tomato2, ast.tomato2rect)
     windowSurface.blit(ast.tomato3, ast.tomato3rect)
-    pygame.draw.rect(windowSurface, cst.BLUE, cst.P1)
-    pygame.draw.rect(windowSurface, cst.BLUE, cst.P2)
-    pygame.draw.rect(windowSurface, cst.BLUE, cst.P3)
-    pygame.draw.rect(windowSurface, cst.BLUE, cst.P4)
-    drawText('Health: %s' % cst.HEALTH, ast.font, windowSurface, cst.WHITE, 10, 10)
-    drawText("Time Score: %s" % cst.BONUS, ast.font, windowSurface, cst.WHITE, 10, 45)
+    pygame.draw.rect(windowSurface, BLUE, P1)
+    pygame.draw.rect(windowSurface, BLUE, P2)
+    pygame.draw.rect(windowSurface, BLUE, P3)
+    pygame.draw.rect(windowSurface, BLUE, P4)
+    drawText('Health: %s' % HEALTH, ast.font, windowSurface, WHITE, 10, 10)
+    drawText("Time Score: %s" % BONUS, ast.font, windowSurface, WHITE, 10, 45)
     pygame.display.flip()
-    windowSurface.fill(cst.WHITE)
+    windowSurface.fill(WHITE)
 
 
 def y_trajectory(y_basis, y_speed, t):
@@ -66,7 +110,7 @@ def displayScreen(x):
 
 def displayWinScreen(x):
     windowSurface.blit(x, (0, 0))
-    drawText("%s" % cst.finalscore, ast.font, windowSurface, cst.WHITE, 335, 107.5)
+    drawText("%s" % finalscore, ast.font, windowSurface, WHITE, 335, 107.5)
     pygame.display.update()
     waitForPlayerToPressKey()
 
